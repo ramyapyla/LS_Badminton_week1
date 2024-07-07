7341495be37b77f21b16327da65157bf0db66259
ramyapyla <ramyadoodle.1@gmail.com>
2024-07-07 21:34:00 +0530

    Add files via upload

---
+++ week1_sports/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navList = document.getElementById('nav-list');
+
+    menuIcon.addEventListener('click', function() {
+        navList.classList.toggle('show');
+    });
+});
+
